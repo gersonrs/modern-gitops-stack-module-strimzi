94ca5012799edc8ee0233fda1e371881c076ae93
Gerson <gersonrodriguessantos8@gmail.com>
2025-01-18 19:12:13 -0300

    fix: fix var name

--- locals.tf
+++ locals.tf
@@ -3,8 +3,8 @@ locals {
     strimzi-kafka-operator = {
       replicas = var.replicas
       resources = {
-        requests = { for k, v in var.resources.controller.requests : k => v if v != null }
-        limits   = { for k, v in var.resources.controller.limits : k => v if v != null }
+        requests = { for k, v in var.resources.general.requests : k => v if v != null }
+        limits   = { for k, v in var.resources.general.limits : k => v if v != null }
       }
     }
   }]
