9006bc1853bdc32bcd679ebfa0ee8186a1bbc081
Gerson <gersonrodriguessantos8@gmail.com>
2025-01-18 19:07:08 -0300

    feat: update chart and update project

--- locals.tf
+++ locals.tf
@@ -1,11 +1,10 @@
 locals {
   helm_values = [{
     strimzi-kafka-operator = {
-      replicas = 1
+      replicas = var.replicas
       resources = {
-        limits = {
-          memory = "500Mi"
-        }
+        requests = { for k, v in var.resources.controller.requests : k => v if v != null }
+        limits   = { for k, v in var.resources.controller.limits : k => v if v != null }
       }
     }
   }]
